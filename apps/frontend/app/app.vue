1e331eecccb4c4e9f2144e545451cc5c2da80d0b
lnx645 <dadanhidyt@gmail.com>
2026-03-06 03:21:28 +0700

    refactor: simplify app layout and add index page template

--- apps/frontend/app/app.vue
+++ apps/frontend/app/app.vue
@@ -1,22 +1,6 @@
 <template>
-  <div>
-    <h1>Daftar User</h1>
-    <pre v-if="!pending">{{ data }}</pre>
-    <div v-else>
-      Loading....
-    </div>
-  </div>
+  <NuxtLoadingIndicator/>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
-
-<script setup lang="ts">
-const { $api } = useNuxtApp()
-
-// Gunakan useAsyncData untuk fetch data di Server Side (SSR)
-const { data, error,pending } = await useAsyncData("usersq", async () => {
-  // Tambahkan .get() di akhir untuk mengeksekusi request
-  const response = await $api.api.users({params : {id:23}})
- 
-  return response;
-})
-
-</script>
